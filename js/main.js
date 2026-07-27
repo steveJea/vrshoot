76f57bea26d5f724c42bfbafa585b7117f898940
STEVE <45488402+steveJea@users.noreply.github.com>
2026-07-27 18:41:11 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -102,8 +102,9 @@ function endBattle(playerWinRound){
   clearInterval(timerInterval);
   window.onkeydown = null;
   window.onkeyup = null;
-  stopBgm(true);
-
+// 매치가 끝났을 때만 BGM 정지 (다음 라운드면 유지)
+  if(matchScore.p >= 2 || matchScore.e >= 2){
+    stopBgm(true);
   if(playerWinRound) matchScore.p++;
   else matchScore.e++;
   updateRoundInfo();
